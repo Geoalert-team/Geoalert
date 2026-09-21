273ccab6d4e82ebf9682809d18c92303d78bc787
Joshua Arcillas <joshuaarcillas8@gmail.com>
2026-09-21 14:21:45 +0800

    unfix: logout functionality

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -10,3 +10,8 @@ LOGGING = {
     'handlers': {'console': {'class': 'logging.StreamHandler'}},
     'root': {'handlers': ['console'], 'level': 'DEBUG'},
 }
+
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
